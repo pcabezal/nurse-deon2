871d7a49adc1db7575fc01783c59d5172508d837
paulcabezal@gmail.com <paulcabezal@gmail.com>
2024-07-18 16:45:35 -0400

    slideshow

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,6 +18,23 @@ var settings = {
 
 };
 
+/** headline image slider */
+
+const images = document.querySelectorAll('.fade-image');
+let currentIndex = 0;
+
+function showNextImage() {
+    images[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % images.length;
+    images[currentIndex].classList.add('active');
+}
+
+setInterval(showNextImage, 5000); // Change image every 5 seconds
+
+// Initialize the first image as visible
+images[currentIndex].classList.add('active');
+
+
 /**
  * Custom banner slider for Apex.
  * @return {jQuery} jQuery object.
